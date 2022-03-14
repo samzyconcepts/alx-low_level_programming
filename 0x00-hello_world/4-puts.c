2b549083cf030905a0f678901840aff413bb0a54
Oderinwale Samson <samzyolufemi@gmail.com>
2022-03-14 05:26:35 -0700

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-/*
- * main - prints the phrase "Programmming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
