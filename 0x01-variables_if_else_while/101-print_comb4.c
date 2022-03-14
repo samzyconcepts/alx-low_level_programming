967aba0b031ba2256cba616b3521e3c5ec44526b
Oderinwale Samson <samzyolufemi@gmail.com>
2022-03-14 13:31:10 -0700

    variables and if

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n, m, p;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (p = 50; p < 58; p++)
+			{
+				if (p > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
