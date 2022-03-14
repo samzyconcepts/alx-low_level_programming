72cabfd1a7316d35a2978d14c8a6d85c2e672d24
Oderinwale Samson <samzyolufemi@gmail.com>
2022-03-14 05:07:59 -0700

    low level language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/**
+/*
  * main - prints the phrase "Programmming is like building a multilingual puzzle
- * Return : 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
